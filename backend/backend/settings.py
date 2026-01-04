b20b66d64c87133e9b66fd33c599d640686e54c1
zitrezy <east_towest@mail.ru>
2026-01-04 20:24:42 +0500

    fix 13

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definitio
